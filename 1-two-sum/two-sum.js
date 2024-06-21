8ba1e53953fc545d814e7615acf0cd2a5d277f0c
soo-yean <soooo.jang@gmail.com>
2024-06-21 10:02:58 +0900

    Time: 112 ms (23.94%) | Memory: 50.2 MB (55.58%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let output = []
+
+    for (let i = 0; i<nums.length; i++) {
+        for (let j = i+1; j <nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                output.push(i,j)
+            }
+        }
+    }
+
+    return output;
+};
