f567e33bdc5998a3eee0c620d43b9106e9954b79
soo-yean <soooo.jang@gmail.com>
2024-06-23 13:21:00 +0900

    TIL: BigInt
    BigInt is specifically designed to work with very large integers

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -7,3 +7,9 @@ var plusOne = function(digits) {
 
     return digitPlusOne.toString().split(''); 
 };
+
+/**
+ * My first attempt was using parseInt, but it failed the test where the digit becomes extremely large.
+ * It was because JavaScript's parseInt function have limitations on the size of integers it can handle.
+ * So I had to switch to using BigInt, which can handle arbitrarily large integers without losing precision.
+ */
