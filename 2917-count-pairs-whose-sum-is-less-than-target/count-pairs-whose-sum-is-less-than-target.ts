062e182374b68318d5e6c5c7f504cde5adcf5dc3
Soo-Yean Jang <soooo.jang@gmail.com>
2024-07-18 14:05:36 +0900

    Time: 65 ms (69.53%) | Memory: 52 MB (46.35%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.ts
@@ -0,0 +1,11 @@
+function countPairs(nums: number[], target: number): number {
+    let pairs = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) pairs++; 
+        }
+    }
+
+    return pairs;
+};
