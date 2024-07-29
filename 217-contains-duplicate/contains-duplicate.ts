4e4377ce9bac0387b41b166cfd6cddac46f5acf6
Soo-Yean Jang <soooo.jang@gmail.com>
2024-07-29 09:23:08 +0900

    Time: 142 ms (7.79%) | Memory: 59.8 MB (98.41%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -0,0 +1,11 @@
+function containsDuplicate(nums: number[]): boolean {
+
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) return true;
+    }
+
+    return false;
+};
+
