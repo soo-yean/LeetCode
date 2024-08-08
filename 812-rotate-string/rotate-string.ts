ac0689fbe6e02c98dc39ebc703a2409e1fb8b551
soo-yean <soooo.jang@gmail.com>
2024-08-08 11:24:16 +0900

    Added thinking process of rotate-string problem

--- 812-rotate-string/rotate-string.ts
+++ 812-rotate-string/rotate-string.ts
@@ -1,3 +1,8 @@
 function rotateString(s: string, goal: string): boolean {
     return s.length === goal.length && s.concat(s).includes(goal);
 };
+
+/** 
+ * The description says: A shift on s consists of moving the leftmost character of s to the rightmost position.
+ * So if we merge the string into itself, it will have all the possible cases of the result of shifting.
+*/
