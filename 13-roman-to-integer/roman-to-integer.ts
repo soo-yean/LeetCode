ad355668be076ec877e5fbfdf4d8b2ba1cb7ad39
soo-yean <soooo.jang@gmail.com>
2024-08-12 09:10:58 +0900

    Added breakdown Roman To Integer

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -1,4 +1,6 @@
 function romanToInt(s: string): number {
+
+    // Create a mapping of Roman numerals to integers
     const romanToIntMap: {[key: string]: number} = {
         'I': 1,
         'V': 5,
@@ -9,14 +11,27 @@ function romanToInt(s: string): number {
         'M': 1000
     }
 
-    let sum = 0;
+    let sum = 0; // Initialize a variable to hold the total value
+
+    // Iterate through the string and calculate the total
     for (let i = 0; i < s.length; i++) {
         const curr = romanToIntMap[s[i]];
         const next = romanToIntMap[s[i + 1]];
 
+        /** Loop through the string and add the value of each Roman numeral to the total.
+         * if a smaller numeral comes before a larger one like 'IV' or 'IX', 
+         *  a) subtract the smaller numeral from the total instead of adding it, or
+         *  b) add (next - curr) value to the total and move on to next numeral
+         * */
         if (next && curr < next) {
-            sum += (next - curr);
-            i++;
+            sum -= curr; // solution (a)
+
+            /**
+             * solution (b) 
+             * sum += (next - curr);
+             * i++
+            */
+           
         } else {
             sum += curr;
         }
