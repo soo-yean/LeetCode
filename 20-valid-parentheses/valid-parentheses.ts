4fdb7daaeda20a29861ff12104e9185694861431
soo-yean <soooo.jang@gmail.com>
2024-08-14 13:46:51 +0900

    Added breakdown Valid Parenthese

--- 20-valid-parentheses/valid-parentheses.ts
+++ 20-valid-parentheses/valid-parentheses.ts
@@ -1,5 +1,5 @@
 function isValid(s: string): boolean {
-    let stack:string[] = [];
+    let stack:string[] = []; //using stack because the order matters.
     const charMap: {[key: string]: string} = {
         ')' : '(',
         '}' : '{',
@@ -11,6 +11,11 @@ function isValid(s: string): boolean {
         if (char === '(' || char === '{' || char === '[') {
             stack.push(char);
         } else {
+            /**
+             * if the current character is closing parenthesis/brace/bracket, check if...
+             * 1) the stack is empty. If it is, return false because it will never be valid without opening.
+             * 2) the top of the stack is opening character which matches with the current one.
+             */
             if (stack.length === 0 || stack.pop() !== charMap[char]) {
                 return false;
             }
