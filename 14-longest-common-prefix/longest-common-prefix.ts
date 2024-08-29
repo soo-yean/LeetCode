125c9caad0a1e10650c252662c6cdaee29be5438
soo-yean <soooo.jang@gmail.com>
2024-08-29 11:06:11 +0900

    Added explanation for Longest Common Prefix

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,7 +1,13 @@
 function longestCommonPrefix(strs: string[]): string {
-    let prefix = strs[0];
+
+    /** assume the first string is the prefix and compare with other strings in the given array */
+    let prefix = strs[0]; 
 
     for (let i = 1; i < strs.length; i++) {
+
+        /** if the current prefix is not at the start of the string,
+         * reduce the prefix by slicing off the last character until it matches
+         * or return "" if the string becomes empty. */
         while (strs[i].indexOf(prefix) !== 0) {
             prefix = prefix.slice(0, -1);
             if (prefix === '') return '';
