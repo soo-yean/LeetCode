3f77aa799d9efc19147266b9e5d0e16a7f32e19e
Soo-Yean Jang <soooo.jang@gmail.com>
2025-06-19 13:54:43 +0900

    Time: 41 ms (57.14%) | Memory: 54 MB (13.10%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -3,6 +3,12 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
+    // let count = 0;
+
+    // for (let i = 0; i < args.length; i++) count++;
+
+    // return count;
+
     return args.length;
 };
 
