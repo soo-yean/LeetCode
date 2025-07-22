83df9ad163177d2dee26f360e9998d7bfd170d26
Soo-Yean Jang <soooo.jang@gmail.com>
2025-07-22 15:48:05 +0900

    Time: 55 ms (39.03%) | Memory: 55.4 MB (5.84%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,13 +4,7 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    /**
-    * //await promise1 waits for promise1 to resolve, and await promise2 will execute and wait again. Then it adds the two results
-    * return await promise1 + await promise2; 
-    */
-
-    // Promise.all runs both promises in parallel(await waits until both are resolved, but they run concurrently).    
-    const [val1, val2] = await Promise.all([promise1, promise2]); 
+    const [val1, val2] = await Promise.all([promise1, promise2])
     return val1 + val2;
 };
 
