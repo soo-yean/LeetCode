3e9057f59b0cb685d4ead9b16ab6e2460a7c4516
Soo-Yean Jang <soooo.jang@gmail.com>
2025-07-22 15:58:22 +0900

    Time: 1 ms (33.90%) | Memory: 57.5 MB (52.49%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.ts
@@ -0,0 +1,15 @@
+function differenceOfSums(n: number, m: number): number {
+    let num1: number = 0;
+    let num2: number = 0;
+
+
+    for (let i = 1; i <= n; i++) {
+        if (i % m !== 0) {
+            num1 += i;
+        } else {
+            num2 += i;
+        }
+    }
+
+    return num1 - num2;
+};
