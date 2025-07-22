0a4e741b1f492918d83260c71c42e15c5171d531
Soo-Yean Jang <soooo.jang@gmail.com>
2025-07-22 16:09:06 +0900

    Time: 1 ms (70.28%) | Memory: 60.3 MB (39.62%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.ts
+++ 2048-build-array-from-permutation/build-array-from-permutation.ts
@@ -2,8 +2,8 @@ function buildArray(nums: number[]): number[] {
     let result: number[] = [];
 
     for (let i = 0; i < nums.length; i++) {
-        // let idx = nums[i];
-        result.push(nums[nums[i]]);
+        let idx = nums[i];
+        result.push(nums[idx]);
     }
 
     return result;
