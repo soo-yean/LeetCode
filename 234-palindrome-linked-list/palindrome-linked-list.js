1b1c47d1c9f199736f1c60dd9ec904afa9a7be83
Soo-Yean Jang <soooo.jang@gmail.com>
2025-11-05 12:42:12 +0100

    Time: 4 ms (81.74%) | Memory: 79.3 MB (47.88%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    let slow = head;
+    let fast = head;
+
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    let prev = null;
+
+    while (slow) {
+        let next = slow.next;
+
+        slow.next = prev;
+
+        prev = slow;
+        slow = next;
+    }
+
+    while(prev) {
+        if (prev.val !== head.val) {
+           return false;
+        } else {
+            prev = prev.next;
+            head = head.next;
+        }
+    }
+    return true;
+};
