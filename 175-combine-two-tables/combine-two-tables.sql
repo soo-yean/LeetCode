a51d83b961fb8a551abe3162c9795bd7275fef7a
Soo-Yean Jang <soooo.jang@gmail.com>
2025-12-29 10:10:10 +0100

    Time: 365 ms (80.50%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT Person.firstName, Person.lastName, Address.city, Address.state 
-FROM Person
-LEFT JOIN Address ON Person.personId = Address.personId
+SELECT person.firstName, person.lastName, address.city, address.state
+FROM person
+LEFT JOIN address
+ON person.personID = address.personID;
